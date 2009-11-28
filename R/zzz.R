36fb180e44d0030032d2db5aad949f1ba9a1b0fc
Katharine Mullen <katharine.mullen@nist.gov>
2009-11-28 00:00:00 +0000

    version 2.0-3

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-".onLoad" <- function (lib, pack)
-{
-  library.dynam(pack, pack, lib)
-  cat("\nDEoptim package")
-  cat("\nDifferential Evolution algorithm in R")
-  cat("\nAuthors: David Ardia and Katharine Mullen\n")
-}
